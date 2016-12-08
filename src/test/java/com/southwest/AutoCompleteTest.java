1a7e9f4b4edcc9b2273624cb3de52882e6838311
Darrell Grainger <dgrainge@thoughtworks.com>
2016-12-08 00:49:33 -0500

    Removed unnecessary import statements.

--- src/test/java/com/southwest/AutoCompleteTest.java
+++ src/test/java/com/southwest/AutoCompleteTest.java
@@ -8,8 +8,6 @@ import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
-import org.openqa.selenium.safari.SafariDriver;
-import org.openqa.selenium.safari.SafariOptions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
